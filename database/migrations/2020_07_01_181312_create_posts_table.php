b14f57c71f1d1f4e249644b85548224ddd66e4b7
janmoes96 <janmoes96@live.nl>
2020-07-02 18:39:54 +0200

    Removals
    Removed $table->date('date'); in Post Migration
    -unneccesary line of code

--- database/migrations/2020_07_01_181312_create_posts_table.php
+++ database/migrations/2020_07_01_181312_create_posts_table.php
@@ -15,7 +15,6 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->date('date');
             $table->timestamps();
         });
     }
